1d2f2d473597a9cb43812d4a5b5fedc87ecd0824
e1ect <e1ectt96@gmail.com>
2021-03-19 23:43:01 +0300

    Закончил оживлять модальные окна

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ const modalMapClose = modalMap.querySelector(".modal__close-btn--map");
 const modalFeedbackLink = document.querySelector(".button--feedback");
 const modalFeedback = document.querySelector(".modal__feedback");
 const modalFeedbackClose = modalFeedback.querySelector(".modal__close-btn--feedback");
+const feedbackForm = modalFeedback.querySelector(".feedback-form");
+const feedbackName = modalFeedback.querySelector(".feedback-form__field--name");
+const feedbackEmail = modalFeedback.querySelector(".feedback-form__field--email");
+const feedbackText = modalFeedback.querySelector(".feedback-form__field--text");
 
 /*Открытие модального окна карты*/
 modalMapLink.addEventListener("click", function (evt) {
@@ -21,10 +25,36 @@ modalMapClose.addEventListener("click", function (evt) {
 modalFeedbackLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalFeedback.classList.add("modal--show");
+  feedbackName.focus();
 })
 
 /*Закрытие модального окна обратной связи*/
 modalFeedbackClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalFeedback.classList.remove("modal--show")
+  modalFeedback.classList.remove("modal-error");
 })
+
+/*Валидация формы*/
+feedbackForm.addEventListener("submit", function (evt) {
+  if (!feedbackName.value || !feedbackEmail.value || !feedbackText.value) {
+    evt.preventDefault();
+    modalFeedback.classList.remove("modal-error");
+    modalFeedback.offsetWidth = modalFeedback.offsetWidth;
+    modalFeedback.classList.add("modal-error");
+  }
+})
+
+/*Закрытие модального окна по нажатию esc*/
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (modalFeedback.classList.contains("modal--show")) {
+      evt.preventDefault();
+      modalFeedback.classList.remove("modal--show");
+    }
+    if (modalMap.classList.contains("modal--show")) {
+      evt.preventDefault();
+      modalMap.classList.remove("modal--show");
+    }
+  }
+});
