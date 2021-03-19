b0a54c88ff71eb93f4c0d5f0f34cd85c7042132d
e1ect <e1ectt96@gmail.com>
2021-03-19 21:53:45 +0300

    Добавил интерактивную карту

--- js/script.js
+++ js/script.js
@@ -5,21 +5,25 @@ const modalFeedbackLink = document.querySelector(".button--feedback");
 const modalFeedback = document.querySelector(".modal__feedback");
 const modalFeedbackClose = modalFeedback.querySelector(".modal__close-btn--feedback");
 
+/*Открытие модального окна карты*/
 modalMapLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalMap.classList.add("modal--show");
 });
 
+/*Закрытие модального окна карты*/
 modalMapClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalMap.classList.remove("modal--show");
 });
 
+/*Открытие модального окна обратной связи*/
 modalFeedbackLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalFeedback.classList.add("modal--show");
 })
 
+/*Закрытие модального окна обратной связи*/
 modalFeedbackClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalFeedback.classList.remove("modal--show")
